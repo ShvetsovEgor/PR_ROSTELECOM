67ed733b7b13d0c09148742795ba33f06e939921
Bungeen <lego-mike@mail.ru>
2023-03-24 10:01:13 +0400

    Registration+TesterAlgorithm

--- vk_bot_test_first.py
+++ vk_bot_test_first.py
@@ -8,12 +8,19 @@ import random
 
 def main():
     vk_session = vk_api.VkApi(
-        token="
+        token="print(123123)
     longpoll = VkBotLongPoll(vk_session, "219545110")
     is_registered = True
     start_reg = False
     is_playing = False
-    qwestion_base = []
+    # id - Text - Type - Media - Type_ans - Correct_ans - Name_film - Link
+    base = {'1': ["Question", "picture", "1.jpg", "qw;er;ty", "er", "FILMMM", "HTTP://"],
+            '2': ["Question", "picture", "2.jpg", "qw;er;ty", "er", "FILMMM", "HTTP://"],
+            '3': ["Question", "picture", "3.jpg", "qw;er;ty", "er", "FILMMM", "HTTP://"]}
+    current_question = None
+    used_questions = []
+    taken_questions = 0
+    correct_ans = 0
     for event in longpoll.listen():
         vk = vk_session.get_api()
 
@@ -71,7 +78,7 @@ def main():
                                          message="Вам необходимо зарегистрироваться",
                                          keyboard=keyboard.get_keyboard(),
                                          random_id=random.randint(0, 2 ** 64))
-        elif not is_playing:
+        elif not current_question:
             if event.type == VkBotEventType.MESSAGE_NEW:
                 if event.obj.message['text'] == 'Правила':
                     keyboard = VkKeyboard(one_time=True)
@@ -83,36 +90,44 @@ def main():
                                      keyboard=keyboard.get_keyboard(),
                                      random_id=random.randint(0, 2 ** 64))
                 elif event.obj.message['text'] == 'Играть':
-                    # id - Text - Type - Media - Type_ans - Correct_ans - Name_film - Link
-                    base = [(1, "Question", "picture", "1.jpg", "qw;er;ty", "FILMMM", "HTTP://"),
-                            (2, "Question", "picture", "2.jpg", "qw;er;ty", "FILMMM", "HTTP://"),
-                            (3, "Question", "picture", "3.jpg", "qw;er;ty", "FILMMM", "HTTP://")]
-                    qwestion_base = random.choices(base, k=2)
-                    random.shuffle(qwestion_base)
-                    is_playing = True
-                    tmp = qwestion_base[0]
+                    if len(used_questions) + 2 > len(list(base.keys())):
+                        keyboard = VkKeyboard(one_time=True)
+
+                        keyboard.add_button('Правила', color=VkKeyboardColor.SECONDARY)
+                        keyboard.add_button('Играть', color=VkKeyboardColor.POSITIVE)
+                        vk.messages.send(user_id=event.obj.message['from_id'],
+                                         message="Попытки закончились. Выберите дальнейшие действия:",
+                                         keyboard=keyboard.get_keyboard(),
+                                         random_id=random.randint(0, 2 ** 64))
+                        continue
+                    taken_questions += 1
+                    current_question = random.choices(list(base.keys()))[0]
+                    while current_question in used_questions:
+                        current_question = random.choices(list(base.keys()))[0]
+                    used_questions += [current_question]
+                    print(current_question)
                     keyboard = VkKeyboard(one_time=True)
-                    answers = list(tmp[4].split(';'))
+                    tmp = base[current_question]
+                    answers = list(tmp[3].split(';'))
                     random.shuffle(answers)
                     keyboard.add_button(answers[0], color=VkKeyboardColor.SECONDARY)
                     keyboard.add_button(answers[1], color=VkKeyboardColor.SECONDARY)
                     keyboard.add_button(answers[2], color=VkKeyboardColor.SECONDARY)
-                    if tmp[2] == 'picture':
+                    if tmp[1] == 'picture':
                         upload = vk_api.VkUpload(vk)
-                        photo = upload.photo_messages(tmp[3])
+                        photo = upload.photo_messages(tmp[2])
                         owner_id = photo[0]['owner_id']
                         photo_id = photo[0]['id']
                         access_key = photo[0]['access_key']
                         attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-                        print(2313213123)
                         vk.messages.send(user_id=event.obj.message['from_id'],
-                                         message=f"{tmp[1]}",
-                                         attachment=attachment,
+                                         message=f"{tmp[0]}",
+                                         attachment="video-219545110_456239017%2Fd1fa5134157be6dce9", #attachment,
                                          keyboard=keyboard.get_keyboard(),
                                          random_id=random.randint(0, 2 ** 64))
                     else:
                         vk.messages.send(user_id=event.obj.message['from_id'],
-                                         message=f"{tmp[1]}",
+                                         message=f"{tmp[0]}",
                                          keyboard=keyboard.get_keyboard(),
                                          random_id=random.randint(0, 2 ** 64))
                 else:
@@ -124,7 +139,54 @@ def main():
                                      message="Выберите:",
                                      keyboard=keyboard.get_keyboard(),
                                      random_id=random.randint(0, 2 ** 64))
-        elif is_playing:
-            pass
+        elif current_question:
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                tmp = base[current_question]
+                if tmp[4] == event.obj.message['text']:
+                    correct_ans += 1
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=f"Верно! {tmp[5]} {tmp[6]}",
+                                     random_id=random.randint(0, 2 ** 64))
+                else:
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=f"Не верно! {tmp[5]} {tmp[6]}",
+                                     random_id=random.randint(0, 2 ** 64))
+                if taken_questions >= 2:
+                    current_question = None
+                    taken_questions = 0
+                    keyboard = VkKeyboard(one_time=True)
+
+                    keyboard.add_button('Правила', color=VkKeyboardColor.SECONDARY)
+                    keyboard.add_button('Играть', color=VkKeyboardColor.POSITIVE)
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=f"Ваш результат: {correct_ans}",
+                                     keyboard=keyboard.get_keyboard(),
+                                     random_id=random.randint(0, 2 ** 64))
+                    correct_ans = 0
+                else:
+                    taken_questions += 1
+                    current_question = random.choices(list(base.keys()))[0]
+                    while current_question in used_questions:
+                        current_question = random.choices(list(base.keys()))[0]
+                    used_questions += [current_question]
+                    keyboard = VkKeyboard(one_time=True)
+                    tmp = base[current_question]
+                    answers = list(tmp[3].split(';'))
+                    random.shuffle(answers)
+                    keyboard.add_button(answers[0], color=VkKeyboardColor.SECONDARY)
+                    keyboard.add_button(answers[1], color=VkKeyboardColor.SECONDARY)
+                    keyboard.add_button(answers[2], color=VkKeyboardColor.SECONDARY)
+                    if tmp[1] == 'picture':
+                        upload = vk_api.VkUpload(vk)
+                        photo = upload.photo_messages(tmp[2])
+                        owner_id = photo[0]['owner_id']
+                        photo_id = photo[0]['id']
+                        access_key = photo[0]['access_key']
+                        attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+                        vk.messages.send(user_id=event.obj.message['from_id'],
+                                         message=f"{tmp[0]}",
+                                         attachment=attachment,
+                                         keyboard=keyboard.get_keyboard(),
+                                         random_id=random.randint(0, 2 ** 64))
 
 main()
